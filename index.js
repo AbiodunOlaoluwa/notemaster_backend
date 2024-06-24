95ed789658a69ec9c03f1fefcffa31671ed26b1e
AbiodunOlaoluwa <mate.olaoluwa@gmail.com>
2024-06-24 12:53:58 +0100

    Set the sameSite cookie parameter to Lax - localhost still works

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(session({
     saveUninitialized: true,
     rolling: true,
     cookie: {
-        sameSite: process.env.NODE_ENV === "PRODUCTION" ? "None" : "Lax",
+        sameSite: "Lax",
         secure: process.env.NODE_ENV === "PRODUCTION",
         maxAge: 3600000,
         httpOnly: true,
